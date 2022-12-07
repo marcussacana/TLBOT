e7e058f8cc9fa773249bb1c3699e472825f33aa4
Marcus André <ddtank.marcus@gmail.com>
2022-12-07 19:34:34 -0300

    Upgrade Version

--- TransBot/Properties/AssemblyInfo.cs
+++ TransBot/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TLBOT 2021")]
-[assembly: AssemblyDescription("Translation Bot 2022 version")]
+[assembly: AssemblyTitle("TLBOT 2023")]
+[assembly: AssemblyDescription("Translation Bot 2023 version")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VNX+")]
-[assembly: AssemblyProduct("TLBOT 2022")]
-[assembly: AssemblyCopyright("Copyright © TLBOT 2016-2022")]
+[assembly: AssemblyProduct("TLBOT 2023")]
+[assembly: AssemblyCopyright("Copyright © TLBOT 2016-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
